434b13d1bdfa3433f80476d277115aa94cc05b77
mischa-saunders <mischa.saunders@gmail.com>
2017-03-05 12:48:09 +1300

    make lat lang numbers

--- db/seeds/farms.js
+++ db/seeds/farms.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('farms').insert({id: 1, name: 'Skilleby Trädgård', location: 'Sweden', type: 'Small Holding', description: 'A small market garden farm specializing in biodynamic agriculture', mainImage: 'http://www.skillebytradgard.se/wp-content/uploads/2016/06/cropped-SK-TR-LOGO-kopia-3.png', latitude: '59.0442751', longitude: '17.5959056'}),
-        knex('farms').insert({id: 2, name: 'Triple-H-Beef', location: 'Canada', type: 'Cattle Ranch', description: 'A family run cattle ranch managed with holistic management techniques', mainImage: 'http://www.triple-h-beef.com/images/grazing-cows-for-email.jpg', latitude: '50.9785995', longitude: '256'}),
-        knex('farms').insert({id: 3, name: 'Tablehurst', location: 'England', type: 'Mixed Use', description: 'A large biodynamic farm and training center', mainImage: 'http://tablehurst.farm/wp-content/uploads/2014/02/tablehurst-video-web-768x316.jpg', latitude: '51.0984584', longitude: '0.0442316'})
+        knex('farms').insert({id: 1, name: 'Skilleby Trädgård', location: 'Sweden', type: 'Small Holding', description: 'A small market garden farm specializing in biodynamic agriculture', mainImage: 'http://www.skillebytradgard.se/wp-content/uploads/2016/06/cropped-SK-TR-LOGO-kopia-3.png', latitude: 59.0442751, longitude: 17.5959056}),
+        knex('farms').insert({id: 2, name: 'Triple-H-Beef', location: 'Canada', type: 'Cattle Ranch', description: 'A family run cattle ranch managed with holistic management techniques', mainImage: 'http://www.triple-h-beef.com/images/grazing-cows-for-email.jpg', latitude: 50.9785995, longitude: 256}),
+        knex('farms').insert({id: 3, name: 'Tablehurst', location: 'England', type: 'Mixed Use', description: 'A large biodynamic farm and training center', mainImage: 'http://tablehurst.farm/wp-content/uploads/2014/02/tablehurst-video-web-768x316.jpg', latitude: 51.0984584, longitude: 0.0442316})
       ])
     })
 };
