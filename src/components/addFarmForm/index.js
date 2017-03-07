93b38eeb9572b0a6daa8b0d76a22f06a8158f9f4
mischa-saunders <mischa.saunders@gmail.com>
2017-03-08 10:05:13 +1300

    basic form rendering

--- src/components/addFarmForm/index.js
+++ src/components/addFarmForm/index.js
@@ -5,16 +5,23 @@ class addFarmForm extends React.Component {
 
   }
   render () {
+    return(
     <div>
-      <form>
+      <form className='AddFarmForm'>
         <input
           placeholder='Farm Name'
           className='input'
           ref='farmName' />
+          <input
+            placeholder='Farm Name'
+            className='input'
+            ref='farmName' />
+
         <button onClick={this.handleSubmit.bind(this)}> Submit
         </button>
       </form>
     </div>
+  )
   }
 }
 module.exports = (addFarmForm)
--- src/containers/home.js
+++ src/containers/home.js
@@ -2,6 +2,7 @@ const React = require('react')
 const { connect } = require('react-redux')
 const Map = require('../components/map')
 const FeaturedFarms = require('../components/featuredFarms')
+const AddFarmForm = require('../components/addFarmForm')
 
 function Home (props) {
   const { dispatch, allFarms } = props
@@ -11,6 +12,7 @@ function Home (props) {
         <Map dispatch={dispatch} allFarms={allFarms} />
       </div>
       <FeaturedFarms allFarms={allFarms} dispatch={dispatch} />
+      <AddFarmForm dispatch={dispatch} />
     </div>
   )
 }
